7e3c52e10db0d1fbda75e2983b3d3090da9d14f2
jorgevarios <jorgevarios@yahoo.com.ar>
2023-01-20 15:31:43 -0300

    Subida de archivo JSON de productos

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.listen(3000, () => {
     console.log("Servidor corriendo en puerto 3000")
 });
