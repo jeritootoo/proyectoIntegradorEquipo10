2d04c669886268457c68bccb7abe1ed9050ffd68
fclaiba <f.claisse.r@gmail.com>
2023-01-29 20:56:11 -0300

    ordenando códigos del controlador y del enrutador de productos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,27 +4,35 @@ const rutaVinosListadoJSON = path.join(__dirname, '../datos/vinoslistado.json');
 const vinos = JSON.parse(fs.readFileSync(rutaVinosListadoJSON, 'utf-8'));
 
 const productsController = { 
+    list: (req, res) => {
+        return res.render('products/listProducts', {vinos: vinos})
+    // Método que renderiza vista del listado de productos  
+    },
     create: (req, res) => {
         return res.render('products/createProducts')
-    // Metodo que renderiza vista de formulario de producto   
-},
+    // Método que renderiza vista de formulario de producto   
+    },
+    detail: (req, res) => {
+        let product = vinos.find(row => row.id == req.params.id)
+        if (product) return res.render("products/detailProducts", {product: product});
+        else return res.send('Producto no encontrado')
+    },
     processCreate: (req, res) => {
         let id = vinos[vinos.length-1].id + 1
         let nuevoVino = {id, ...req.body}
         vinos.push(nuevoVino)
         fs.writeFileSync(rutaVinosListadoJSON, JSON.stringify(vinos, null, 2))
         console.log(req.body)
-        return res.redirect('/products/listProducts')
-        
-},
-    list: (req, res) => {
-        return res.render('products/listProducts', {vinos: vinos})
-    // Metodo que renderiza vista del listado de productos  
-},
-    detail: (req, res) => {
-        let product = vinos.find(row => row.id == req.params.id)
-        if (product) return res.render("products/detailProducts", {product: product});
-        else return res.send('Producto no encontrado')
+        return res.redirect('/products/listProducts') 
+    },
+    edit: (req,res) => {
+        //Código
+    },
+    processEdit: (req,res) => {
+        //Código
+    },
+    delete: (req,res) => {
+        //Código
     }
 };
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,11 +3,19 @@ const express = require("express");
 const productsRouter = express();
 const productsController = require("../controllers/productsController")
 
-productsRouter.get("/createProducts", productsController.create);
-productsRouter.post("/createProducts", productsController.processCreate);
-
 productsRouter.get("/listProducts", productsController.list);
-
+//Código
+productsRouter.get("/createProducts", productsController.create);
+//Código
 productsRouter.get("/detailProducts/:id", productsController.detail);
+//Código
+productsRouter.post("/createProducts", productsController.processCreate);
+//Código
+productsRouter.get('/',productsController.edit);
+//Código
+productsRouter.put('/',productsController.processEdit);
+//Código
+productsRouter.delete('/',productsController.delete);
+//Código
 
 module.exports = productsRouter; 
