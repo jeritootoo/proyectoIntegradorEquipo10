6fe6cca52274156647bb076887d436d2f625a607
fclaiba <f.claisse.r@gmail.com>
2023-03-02 22:48:21 -0300

    encarpetado y archivos base para las vistas + correcciones generales

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 app.use(express.static('public'));
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -10,8 +13,12 @@ app.use(express.urlencoded({extended: false}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.listen(3000, () => {
+app.listen(3000,() => {
 console.log("Servidor corriendo en puerto 3000");
+});
+
+app.get('/', () => {
+res.rend("Bienvenido al sitio")
 });
 
 
